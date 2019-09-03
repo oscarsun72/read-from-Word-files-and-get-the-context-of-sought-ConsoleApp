b992638e65a9baa31ef9ffab8148091c32727349
守真 孫 <oscarsun72@hotmail.com>
2019-09-03 19:38:47 +0800

    加入專案檔案。

---
+++ 讀取Word並從找到的字串擷取該段文字-ConsoleApp/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.Office.Interop.Word
+Module Module1
+    Sub Main()
+        Const fName = "c:\心經.docx"
+        Dim findTxt = "恐怖"
+        Dim doc As Microsoft.Office.Interop.Word.Document
+        'Dim docs As Microsoft.Office.Interop.Word.Documents
+        doc = GetObject("f") '現有的檔案用GetObject即可
+        Dim foundRng As Microsoft.Office.Interop.Word.Range = doc.Range()
+        If foundRng.Find().Execute(findTxt) Then '如果有找到的話
+            foundRng.Select()
+            Console.Write(doc.ActiveWindow.Selection.Paragraphs(1).Range.Text)
+        End If
+        doc.ActiveWindow.Visible = True
+        doc = Nothing
+    End Sub
+End Module
---
+++ 讀取Word並從找到的字串擷取該段文字-ConsoleApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 組件的一般資訊是由下列的屬性集控制。
+' 變更這些屬性的值即可修改組件的相關
+' 資訊。
+
+' 檢閱組件屬性的值
+
+<Assembly: AssemblyTitle("讀取Word並從找到的字串擷取該段文字-ConsoleApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("讀取Word並從找到的字串擷取該段文字-ConsoleApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("2b96bd69-d711-4ae0-b294-ab94d4f4ddaf")>
+
+' 組件的版本資訊由下列四個值所組成: 
+'
+'      主要版本
+'      次要版本
+'      組建編號
+'      修訂編號
+'
+' 您可以指定所有的值，或將組建編號或修訂編號設為預設值
+' 指定為預設值: 
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
