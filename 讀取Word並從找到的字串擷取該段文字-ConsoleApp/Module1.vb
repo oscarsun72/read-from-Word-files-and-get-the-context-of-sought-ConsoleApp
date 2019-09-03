136377dc547356f0a18fcdbf4c2ced275ba7ad65
守真 孫 <oscarsun72@hotmail.com>
2019-09-03 20:06:14 +0800

    correct

--- 讀取Word並從找到的字串擷取該段文字-ConsoleApp/Module1.vb
+++ 讀取Word並從找到的字串擷取該段文字-ConsoleApp/Module1.vb
@@ -5,13 +5,15 @@ Module Module1
         Dim findTxt = "恐怖"
         Dim doc As Microsoft.Office.Interop.Word.Document
         'Dim docs As Microsoft.Office.Interop.Word.Documents
-        doc = GetObject("f") '現有的檔案用GetObject即可
+        doc = GetObject(fName) '現有的檔案用GetObject即可
         Dim foundRng As Microsoft.Office.Interop.Word.Range = doc.Range()
         If foundRng.Find().Execute(findTxt) Then '如果有找到的話
             foundRng.Select()
             Console.Write(doc.ActiveWindow.Selection.Paragraphs(1).Range.Text)
+            Console.ReadLine()
         End If
         doc.ActiveWindow.Visible = True
+        'doc.Close(WdSaveOptions.wdDoNotSaveChanges)'如果要關掉文件，再執行此行
         doc = Nothing
     End Sub
 End Module
